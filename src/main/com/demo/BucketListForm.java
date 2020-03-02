713619f116a6c9dfddc72d82d61a149e23d81a3c
503183144 <61604753+503183144@users.noreply.github.com>
2020-03-02 16:33:43 -0600

    Update and rename CognitoWebConfiguration.java to BucketListForm.java

---
+++ src/main/com/demo/BucketListForm.java
@@ -0,0 +1,51 @@
+package com.ge.cognito.ui;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+class BucketListForm {
+
+    static void display(String title, String message) {
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Scene scene = new Scene(grid, 400, 500);
+
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinWidth(250);
+        TextArea scenetitle = new TextArea();
+
+        scenetitle.setText(message);
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
+        grid.add(scenetitle, 0, 0, 2, 1);
+        Button cancelButton = new Button("OK");
+        HBox clBtn = new HBox(10);
+        clBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        clBtn.getChildren().add(cancelButton);
+        clBtn.setMaxWidth(190);
+        grid.add(clBtn, 1, 6);
+        cancelButton.setOnAction(e -> {
+            window.close();
+        });
+
+
+        window.setScene(scene);
+        window.showAndWait();
+    }
+
+}
